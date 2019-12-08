9b4ad76de3284ebafb3e9fb0bfb12aa471f5720d
Önsel AYDIN <onselaydin@gmail.com>
2019-12-08 15:30:05 +0300

    state notlara devam

---
+++ mapstest/src/context.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const UserContext = React.createContext();
+export class UserProvider extends Component {
+    render() {
+        return (
+            <UserContext.Provider>
+                {this.props.children}
+            </UserContext.Provider>
+        )
+    }
+}
