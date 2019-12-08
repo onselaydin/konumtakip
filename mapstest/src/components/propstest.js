e62308245dc4a421a60e9e81a8b3b9f96aca3c9a
Önsel AYDIN <onselaydin@gmail.com>
2019-12-08 13:59:49 +0300

    react state ve eventler

--- mapstest/src/components/propstest.js
+++ mapstest/src/components/propstest.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
 
 export default class propstest extends Component {
+    state = {
+        test:"Önsel Test",
+        isVisible : false
+    }
+    // constructor(props) {
+    //     super(props)
+    
+    //     this.state = {
+    //          test:"Önsel Test",
+    //          isVisible : false
+    //     }
+    // }
+    onClickEvent(e){
+        console.log(e.target); //Bu eventin nerde oluştuğu bilgisini alırız.
+        console.log("Test");
+        console.log(this);
+    }
     render() {
+        const {isVisible} = this.state;
         return (
             <div>
                 <ul>
@@ -9,6 +27,12 @@ export default class propstest extends Component {
                     <li>Departman : {this.props.department} </li>
                     <li>Maaş : {this.props.salary} </li>
                 </ul>
+                {
+                    isVisible ? <p>{this.state.test}</p>
+                    : null
+                }
+
+                <p onClick={this.onClickEvent.bind(this)}>Event testleri</p>
             </div>
         )
     }
